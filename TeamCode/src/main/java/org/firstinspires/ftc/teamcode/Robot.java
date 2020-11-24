6486439bf70bd91ba7a41f401ad65c99b79e7789
TheRealOP <67725442+TheRealOP@users.noreply.github.com>
2020-11-23 19:34:40 -0500

    Init motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,34 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.sun.tools.corba.se.idl.constExpr.ShiftRight;
 
 import org.firstinspires.ftc.teamcode.autos.Methods;
 
 public class Robot {
 
     // Declaring DT motors
-    DcMotor leftFront, rightFront, leftBack, rightBack;
+    public Motor leftFront, rightFront, leftBack, rightBack;
+
+    public MecanumDrive dt;
 
     // Declare Method object?
-    Methods methods;
+    public Methods methods;
 
     // Hardware map??
-    HardwareMap hardMap;
+    public HardwareMap hardMap;
 
     public Robot(){
+        initRobotHardware();
 
-        // Initialize DT Motors
-        leftFront = hardMap.get(DcMotor.class , "leftFront");
-        rightFront = hardMap.get(DcMotor.class , "rightFront");
-        leftBack = hardMap.get(DcMotor.class , "leftBack");
-        rightBack = hardMap.get(DcMotor.class , "rightBack");
+    }
 
-        //
-        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    public void initRobotHardware(){
+        leftFront = new Motor(hardMap,"leftFront" );
+        rightFront = new Motor(hardMap,"rightFront" );
+        leftBack = new Motor(hardMap,"leftBack" );
+        rightBack = new Motor(hardMap,"rightBack" );
+
+        leftFront.setRunMode(Motor.RunMode.PositionControl);
+        rightFront.setRunMode(Motor.RunMode.PositionControl);
+        leftBack.setRunMode(Motor.RunMode.PositionControl);
+        rightBack.setRunMode(Motor.RunMode.PositionControl);
+
+        dt = new MecanumDrive(leftFront,rightFront,leftBack,rightBack);
+
+        leftFront.setPositionCoefficient(0.05);
+        rightFront.setPositionCoefficient(0.05);
+        leftBack.setPositionCoefficient(0.05);
+        rightBack.setPositionCoefficient(0.05);
+
 
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Robot;
+
+@Autonomous(name = "autoTest",group = "autos")
+
+public class Test extends LinearOpMode {
+
+    Robot robot = new Robot();
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        while (opModeIsActive()){
+            robot.leftFront.setTargetPosition(28*10);
+            robot.rightFront.setTargetPosition(28*10);
+            robot.leftBack.setTargetPosition(28*10);
+            robot.rightBack.setTargetPosition(28*10);
+            while(!(robot.leftFront.atTargetPosition() && robot.rightFront.atTargetPosition() && robot.leftBack.atTargetPosition() && robot.rightBack.atTargetPosition())){
+                robot.leftFront.set(0.25);
+                robot.rightFront.set(0.25);
+                robot.leftBack.set(0.25);
+                robot.rightBack.set(0.25);
+            }
+            robot.leftFront.set(0);
+            robot.rightFront.set(0);
+            robot.leftBack.set(0);
+            robot.rightBack.set(0);
+
+        }
+    }
+}
